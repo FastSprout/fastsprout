83ed6f5df342c6cd9ebf3f701c34894e2d041175
Illia Bahlai <i.bahlai@3dcra.eu>
2026-09-14 15:28:19 +0000

    add base query methods

--- src/fastsprout/data/capabilities/query.py
+++ src/fastsprout/data/capabilities/query.py
@@ -1,16 +1,29 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Annotated, Any, Self
+
+from annotated_types import Ge
 
 from fastsprout.data.entity import Entitieable
 
 
 @dataclass
-class BaseQuery[E: Entitieable, QueryT](ABC):
+class BaseQuery[E: Entitieable[Any], QueryT](ABC):
     """Base for filters."""
 
     entity: type[E]
 
-    @property
+    def limit(self, max_n: Annotated[int, Ge(1)] = 1) -> Self:
+        self._limit_value = max_n
+        return self
+
+    def offset(self, to: Annotated[int, Ge(0)] = 0) -> Self:
+        self._offset_value = to
+        return self
+
     @abstractmethod
-    def builded_query(self) -> QueryT:
-        raise NotImplementedError
+    def _build(self) -> QueryT: ...
+
+    @property
+    def _built_query(self) -> QueryT:
+        return self._build()
