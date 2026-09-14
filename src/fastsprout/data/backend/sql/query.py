9b1d921132dfa41507d5e64d481dc528d2c7b356
Illia Bahlai <i.bahlai@3dcra.eu>
2026-09-14 15:30:58 +0000

    add base impl in sql query

--- src/fastsprout/data/backend/sql/query.py
+++ src/fastsprout/data/backend/sql/query.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from typing import Any, override
+from typing import Any
 
-from sqlalchemy.sql import select
+from sqlalchemy import delete, insert, select, update
+from sqlalchemy.sql import Delete, Insert, Update
 from sqlalchemy.sql.selectable import Select
 
 from fastsprout.data.backend.sql.entity import SQLEntity
@@ -12,7 +13,25 @@ __all__ = ["SQLQuery"]
 
 @dataclass
 class SQLQuery[E: SQLEntity[Any]](BaseQuery[E, Select[tuple[E]]]):
+    def _build(self) -> Select[tuple[E]]:
+        return self._built_select.limit(
+            getattr(self, "_limit_value", None),
+        ).offset(
+            getattr(self, "_offset_value", None),
+        )
+
     @property
-    @override
-    def builded_query(self):
+    def _built_select(self) -> Select[tuple[E]]:
         return select(self.entity)
+
+    @property
+    def _built_insert(self) -> Insert:
+        return insert(self.entity.__table__)
+
+    @property
+    def _built_update(self) -> Update:
+        return update(self.entity.__table__)
+
+    @property
+    def _built_delete(self) -> Delete:
+        return delete(self.entity.__table__)
